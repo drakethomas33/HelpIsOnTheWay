b05d9ccc0b6ef641fb01fa77e024b751e228f2f1
Tom <Thomas@oceanlabs.co>
2014-12-08 16:18:38 +0000

    blank=True

--- project/blog/models.py
+++ project/blog/models.py
@@ -9,9 +9,9 @@ class Article(models.Model):
     STATUS = Choices('draft', 'published')
 
     title = models.CharField(max_length=128)
-    subtitle = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=128, blank=True)
     image = models.ImageField(upload_to='img')
-    teaser = models.TextField()
+    teaser = models.TextField(blank=True)
     content = models.TextField()
     status = StatusField()
     published_at = MonitorField(monitor='status', when=['published'])
